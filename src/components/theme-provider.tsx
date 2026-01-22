ff42a7c4be3fa2ec0855aaeb0d41a4fcd5cc9058
naufaladi <naufaladi10@gmail.com>
2026-01-22 20:38:18 +0700

    ref: code indentation

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,7 +1,10 @@
-"use client"
-import {ThemeProvider as NextThemeProvider, ThemeProviderProps} from "next-themes"
+"use client";
 
-export function ThemeProvider({children, ...props}: ThemeProviderProps) {
-    return <NextThemeProvider {...props}>{children}</NextThemeProvider>
+import {
+  ThemeProvider as NextThemeProvider,
+  ThemeProviderProps,
+} from "next-themes";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
 }
-
